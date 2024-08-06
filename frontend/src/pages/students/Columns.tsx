771978569195fed8c64e80a700e48cb7eca2f7e0
SS-Keerthana <isenkeerthana.senthilkumar@gmail.com>
2024-08-06 22:11:46 +0530

    worked on data-table

--- frontend/src/pages/students/Columns.tsx
+++ frontend/src/pages/students/Columns.tsx
@@ -1,7 +1,17 @@
 // import { ColumnDef } from "@tanstack/react-table"
 
 import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal,ArrowUpDown } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 export type Payment = {
@@ -33,8 +43,32 @@ export type Payment = {
 
 export const columns: ColumnDef<Payment>[] = [
   {
-    accessorKey: "id",
-    header: "id",
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
   {
     accessorKey: "status",
@@ -42,7 +76,17 @@ export const columns: ColumnDef<Payment>[] = [
   },
   {
     accessorKey: "email",
-    header: "Email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
   },
   {
     accessorKey: "amount",
--- frontend/src/pages/students/Student.tsx
+++ frontend/src/pages/students/Student.tsx
@@ -1,8 +1,10 @@
+
 import { Button } from "@/components/ui/button"
 import { UserPlus , CirclePlus } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { DataTable } from "@/components/custom/data-table"
 import { Payment, columns } from "./Columns"
+
 
 
 export const payments: Payment[] = [
@@ -52,7 +54,100 @@ export const payments: Payment[] = [
     status: "processing",
     email: "example@gmail.com",
   },
-  // ...
+  {
+    id: "720ed52f",
+    amount: 100,
+    status: "failed",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed572f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed582f",
+    amount: 100,
+    status: "processing",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed529f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed452f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  },
+  {
+    id: "189e1d42",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  {
+    id: "228ed52f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  },
+  {
+    id: "489e1d43",
+    amount: 125,
+    status: "failed",
+    email: "example@gmail.com",
+  }, {
+    id: "728ed62f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "389e1d72",
+    amount: 125,
+    status: "failed",
+    email: "example@gmail.com",
+  }, {
+    id: "48ed02f",
+    amount: 100,
+    status: "pending",
+    email: "m@example.com",
+  },
+  {
+    id: "589e1d32",
+    amount: 125,
+    status: "processing",
+    email: "example@gmail.com",
+  },
+  {
+    id: "620ed52f",
+    amount: 100,
+    status: "failed",
+    email: "m@example.com",
+  },
+  {
+    id: "728ed5f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  },
+  {
+    id: "828ed582f",
+    amount: 100,
+    status: "processing",
+    email: "m@example.com",
+  },
+  {
+    id: "928ed529f",
+    amount: 100,
+    status: "success",
+    email: "m@example.com",
+  }
 ]
 
 
